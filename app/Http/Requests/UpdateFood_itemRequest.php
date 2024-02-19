bc17c358629b686ee988da869e4d5671cccf03ca
GiacomoFranchi <giacomo.franchi.97@gmail.com>
2024-02-19 16:55:03 +0100

    add: edit & store in Food_itemsController

---
+++ app/Http/Requests/UpdateFood_itemRequest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UpdateFood_itemRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'name' => ['required', 'min:3', 'max:100', Rule::unique('food_items')->ignore($this->food_item)],
+            'image' => ['nullable', 'image', 'max:512'],
+            'description' => ['required'],
+            'price' => ['required','numeric','min:0'],
+            'is_visible' => ['required']
+        ];
+    }
+    public function messages(){
+        return[
+            'name.required' => 'Inserisci il titolo del piatto',
+            'description.required' => 'Inserisci la descrizione o il nass ti fa chiudere',
+            'name.min' => 'Il titolo può avere minimo :min caratteri',
+            'name.max' => 'Il titolo può avere al massimo :max caratteri',
+            'image.image' =>'formato errato',
+            'image.max' =>'dimensione eccessiva',
+            'price.required' => 'Inserisci un prezzo',
+            'price.numeric' => 'solo numeri grazie',
+            'price.min' => 'Non può essere il prezzo giusto...',
+            'is_visible.required' => 'è disponibile o no?'            
+        ];
+    }
+}
