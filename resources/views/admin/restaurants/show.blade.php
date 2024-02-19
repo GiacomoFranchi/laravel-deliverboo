bcc832cfcbb8409570a29233c484106941ecd7d7
EnricoDG97 <enrico.delgaudio97@gmail.com>
2024-02-19 18:05:02 +0100

    feat: added navigation to index, edit and delete button to show of restaurants and food_items

--- resources/views/admin/restaurants/show.blade.php
+++ resources/views/admin/restaurants/show.blade.php
@@ -4,55 +4,59 @@
     <div class="container mt-3 mb-5">
         <h2 class="fs-1 mb-3">{{ $restaurant->name }}</h2>
 
-        
+
         <div>
             <img src="{{ asset('storage/' . $restaurant->image) }}" alt="">
         </div>
-       
+
 
         <hr>
 
         <ul>
             <li class="mt-5 fs-5">
                 <span class="fw-bold ">Address</span>
-                 {{ $restaurant->address }}
-           
-           </li>
+                {{ $restaurant->address }}
+
+            </li>
 
             <li class="mt-2 fs-5">
                 <span class="fw-bold ">VAT number:</span>
-                
+
                 {{ $restaurant->vat_number ? $restaurant->vat_number : 'No VAT number available' }}
             </li>
 
             <li class="mt-2 fs-5">
                 <span class="fw-bold ">Phone Number:</span>
-                
+
                 {{ $restaurant->phone_number }}
             </li>
 
             <li class="mt-2 fs-5">
                 <span class="fw-bold ">Opening Time:</span>
-                
-                {{ date("H:i", strtotime($restaurant->opening_time)) }}
+
+                {{ date('H:i', strtotime($restaurant->opening_time)) }}
             </li>
 
             <li class="mt-2 fs-5">
                 <span class="fw-bold ">Closing Time:</span>
-                
-                {{ date("H:i", strtotime($restaurant->closing_time)) }}
+
+                {{ date('H:i', strtotime($restaurant->closing_time)) }}
             </li>
 
             <li class="mt-2 fs-5">
                 <span class="fw-bold ">Closure Day:</span>
-                
+
                 {{ $restaurant->closure_day }}
             </li>
         </ul>
 
 
-        <a href="{{ route('admin.restaurants.edit', ['restaurant' => $restaurant->slug]) }}" class="btn btn-warning">Edit You
-            Restaurant
+        <a href="{{ route('admin.restaurants.edit', ['restaurant' => $restaurant->slug]) }}" class="btn btn-warning">
+            <i class="fa-solid fa-pencil"></i>
+        </a>
+
+        <a class="btn btn-success" href="{{ route('admin.restaurants.index') }}">
+            View all
         </a>
 
         <form action="{{ route('admin.restaurants.destroy', ['restaurant' => $restaurant->slug]) }}" class="d-inline-block"
@@ -62,12 +66,12 @@
             @method('DELETE')
 
             <button class="btn btn-danger btn-delete" type="submit" data-title="{{ $restaurant->name }}">
-                Delete
+                <i class="fa-solid fa-trash-can"></i>
             </button>
 
         </form>
     </div>
-    
+
     @include('admin.restaurants.partials.delete-modal')
 @endsection
 
