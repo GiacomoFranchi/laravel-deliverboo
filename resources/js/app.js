4bde8bce1c9cc309a486a3f29117494e3f6cd3ef
EnricoDG97 <enrico.delgaudio97@gmail.com>
2024-02-19 16:11:44 +0100

    feat: added delete function to item_food, added button delete and modal delete to partials, added script for modal-delete

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,28 @@ import * as bootstrap from 'bootstrap'
 import.meta.glob([
     '../img/**'
 ])
+
+
+//  start MODAL - DELETE ITEM FOOD
+const archieveButtons = document.querySelectorAll('.btn-delete-food');
+// console.log(archieveButtons);
+
+archieveButtons.forEach(button => {
+    button.addEventListener('click', (event) => {
+        event.preventDefault();
+        // console.log('clicked');
+        
+        const deleteModal = new bootstrap.Modal('#delete-modal');
+        
+        const title = button.getAttribute('data-title');
+        document.getElementById('name-to-delete').innerHTML = title;
+        
+        document.getElementById('button-delete').addEventListener('click', () => {
+            // console.log(button.parentElement);
+            button.parentElement.submit();
+        })
+
+        deleteModal.show();
+    });
+})
+ // end MODAL - DELETE ITEM FOOD
