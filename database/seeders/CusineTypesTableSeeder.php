f20e18705fe698a812ec1ef520d422a2c29be330
ItsKristian01 <kristianhana11@gmail.com>
2024-02-19 11:11:40 +0100

    fix: syntax errors

--- database/seeders/CusineTypesTableSeeder.php
+++ database/seeders/CusineTypesTableSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\Cusine_Type;
+use App\Models\CusineType;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -18,7 +18,7 @@ class CusineTypesTableSeeder extends Seeder
         $cusine_types = ['Italian', 'French', 'Chinese', 'Japanese','Greek','Spanish','Turkey','Thai','Mexican','American '];
 
         foreach ($cusine_types as $cusine_type) {
-            $new_cusine_type= new Cusine_Type();
+            $new_cusine_type= new CusineType;
             $new_cusine_type->name = $cusine_type;
             $new_cusine_type->save();
         }
