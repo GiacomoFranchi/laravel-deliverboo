d6f4c85c51b1919f0a348e12a37f84fe96030a1c
ItsKristian01 <kristianhana11@gmail.com>
2024-02-19 10:53:36 +0100

    add: Pivot table restaurant_cusine_type

---
+++ database/migrations/2024_02_19_094607_create_restaurant_cusine_type_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('restaurant_cusine_type', function (Blueprint $table) {
+            $table->unsignedBigInteger('restaurant_id');
+            $table->foreign('restaurant_id')->references('id')->on('restaurants');
+
+            $table->unsignedBigInteger('cusine_type_id');
+            $table->foreign('cusine_type_id')->references('id')->on('cusine_types');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('restaurant_cusine_type');
+    }
+};
