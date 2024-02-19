0b9c2a785cea2dba4caa1f315ef01447c47c5146
ItsKristian01 <kristianhana11@gmail.com>
2024-02-19 10:45:09 +0100

    add: Relationship on the model

--- app/Models/CusineType.php
+++ app/Models/CusineType.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Cusine_Type extends Model
 {
     use HasFactory;
+    
+    //Relationship with Restaurants (Many-to-Many)
+    public function restaurants(){
+        return $this->belongsToMany(Restaurant::class);
+    }
 }
