d6d70ea12c6be3560953d45773eccaba2c6b6381
EnricoDG97 <enrico.delgaudio97@gmail.com>
2024-02-19 11:30:20 +0100

    add: new fields to html registration form - without validation

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -10,7 +10,8 @@
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
-
+                        
+                        {{-- NAME FIELD --}}
                         <div class="mb-4 row">
                             <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
 
@@ -24,7 +25,17 @@
                                 @enderror
                             </div>
                         </div>
+                        
+                        {{-- LAST_NAME FIELD --}}
+                        <div class="mb-4 row">
+                            <label for="last_name" class="col-md-4 col-form-label text-md-right">{{ __('Last Name') }}</label>
 
+                            <div class="col-md-6">
+                                <input id="last_name" type="text" class="form-control" name="last_name" value="{{ old('last_name') }}" required autocomplete="last_name" autofocus>
+                            </div>
+                        </div>
+                        
+                        {{-- EMAIL FIELD --}}
                         <div class="mb-4 row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
@@ -38,7 +49,17 @@
                                 @enderror
                             </div>
                         </div>
+                        
+                        {{-- ADDRESS FIELD --}}
+                        <div class="mb-4 row">
+                            <label for="address" class="col-md-4 col-form-label text-md-right">{{ __('Address') }}</label>
 
+                            <div class="col-md-6">
+                                <input id="address" type="text" class="form-control" name="address" value="{{ old('address') }}" required autocomplete="address" autofocus>
+                            </div>
+                        </div>
+
+                        {{-- PASSWORD FIELD --}}
                         <div class="mb-4 row">
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
@@ -52,7 +73,8 @@
                                 @enderror
                             </div>
                         </div>
-
+                        
+                        {{-- PASSWORD CONFIRMATION FIELD --}}
                         <div class="mb-4 row">
                             <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
 
@@ -60,7 +82,17 @@
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                             </div>
                         </div>
+                        
+                        {{-- PHONE NUMBER FIELD --}}
+                        <div class="mb-4 row">
+                            <label for="phone_number" class="col-md-4 col-form-label text-md-right">{{ __('Phone Number') }}</label>
 
+                            <div class="col-md-6">
+                                <input id="phone_number" type="text" class="form-control" name="phone_number" value="{{ old('phone_number') }}" required autocomplete="phone_number" pattern="\+?[0-9()\s-]{8,20}">
+                            </div>
+                        </div>
+
+                        {{-- SUBMIT BUTTON --}}
                         <div class="mb-4 row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
