31f7b5785edd3dca22151e4e583bf1a189938642
GiacomoFranchi <giacomo.franchi.97@gmail.com>
2024-02-21 15:58:08 +0100

    add: CusineTypeRestaurantSeeder

---
+++ database/seeders/RestaurantsTableSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Restaurant;
+use App\Models\CusineType;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+class RestaurantsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+
+        for($i=0; $i<15; $i++){
+            $restaurant = new Restaurant();
+            $restaurant->user_id = $i;
+            $restaurant->name = $faker->word();
+            $restaurant->address = $faker->address();
+            $restaurant->vat_number =$faker->numerify('SA###########');
+            $restaurant->phone_number = $faker->phoneNumber();
+            $restaurant->opening_time = $faker->time($format = 'H:i');
+            $restaurant->closing_time = $faker->time($format = 'H:i');
+            $restaurant->closure_day = $faker->dayOfWeek();
+            $restaurant->image = $faker->image(null, 640, 480);
+            $restaurant->save();            
+        }
+    }
+}
