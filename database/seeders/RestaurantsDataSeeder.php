3966ece3f61faaea0e7cba306290022ed5765c1e
serena.peschiaroli <serena.peschiaroli@gmail.com>
2024-02-25 00:26:37 +0100

    Fix: mockdata json, restaurant data seeder for adding images

--- database/seeders/RestaurantsDataSeeder.php
+++ database/seeders/RestaurantsDataSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\CusineType;
 use App\Models\Food_item;
 use App\Models\Restaurant;
+use Carbon\Carbon;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
@@ -23,18 +24,23 @@ class RestaurantsDataSeeder extends Seeder
         $data = json_decode(file_get_contents($jsonPath), true);
 
         foreach ($data['Restaurants'] as $restaurantData) {
+
+            $openingTime = Carbon::createFromTime($faker->numberBetween(6, 11), 0, 0);
+            $closingTime = (clone $openingTime)->addHours(rand(8, 12));
+
+            $imagePath = $restaurantData['image'] ? 'restaurants_images/' . $restaurantData['image'] : null;
+
             $restaurant = Restaurant::updateOrCreate(
                 ['vat_number' => $faker->numerify('IT###########')],
                 [ 
-                    'vat_number' => $faker->numerify('IT###########'),
                     'name' => $restaurantData['name'],
                     'address' => $restaurantData['address'],
                     'phone_number' => $faker->phoneNumber(),
-                    'user_id' =>$faker->numberBetween(1, 4),
-                    'opening_time' => $faker->time($format = 'H:i'),
-                    'closing_time' => $faker->time($format = 'H:i'),
+                    'user_id' =>$faker->numberBetween(1, 3),
+                    'opening_time' => $openingTime->format('H:i:s'),
+                    'closing_time' => $closingTime->format('H:i:s'),
                     'closure_day' => $faker->dayOfWeek(),
-                    'image' => $faker->image(null, 640, 480),
+                    'image' => $imagePath,
                     
                 ]
             );
