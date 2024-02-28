9204dd584e0016291ca20fbacf6f2c2d611d4d6d
GianmarcoDandrea <gianm96@hotmail.it>
2024-02-28 10:29:14 +0100

    chenged: order route in api

--- routes/api.php
+++ routes/api.php
@@ -29,7 +29,7 @@ Route::get('/restaurant/{slug}', [RestaurantController::class, 'show']);
 Route::get('/restaurants/cusine_type/{cusine_types_name}', [RestaurantController::class, 'ShowRestaurantsByCuisineType']);
 Route::get('/cusine_types', [CuisineTypeController::class, 'index']);
 Route::get('/food_items', [FoodItemController::class, 'index']);
-Route::post('/restaurant/{slug}/orders', [OrderController::class, 'store']);
+Route::post('/restaurant/{id}/orders', [OrderController::class, 'store']);
 Route::get('/payment/token', [BrainTreeController::class, 'getToken'])->name('payment.token');
 Route::post('/payment/checkout', [BrainTreeController::class, 'checkout'])->name('payment.checkout');
 
